afb566bf8063c13e9f301464424389e09b689881
Gustavo Novaes <gnovaes@options.qss.com.br>
2017-11-27 09:24:12 -0200

    Adiciona teste a home 27 nov 17

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -1,10 +1,13 @@
 from django.test import TestCase
 
+#testcase já vem com o django
 class HomeTest(TestCase):
     def setUp(self):
+        #self é uma variável de instancia para ficar visivel ns outras funcoes
         self.response = self.client.get('/')
 
     def test_get(self):
+        #Docstring - mensagem que erá retornada pelo teste
         """get / must return status code 200"""
         self.assertEqual(200, self.response.status_code)
 
