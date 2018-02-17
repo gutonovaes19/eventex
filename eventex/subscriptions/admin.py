a5d429507b084c304c8dd14522aeb68f97cd9f57
gutonovaes19 <gutonovaes19@gmail.com>
2018-02-17 16:01:51 -0200

    diaruim

--- eventex/subscriptions/admin.py
+++ eventex/subscriptions/admin.py
@@ -1,12 +1,5 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from eventex.subscriptions.models import Subscription
 
-
 admin.site.register(Subscription)
-=======
-from eventex.subscriptions.models import Subscripion
 
-
-admin.site.register(Subscripion)
->>>>>>> 27f9dbba17fc377078643ac2ec13e6d6bb661f8c
