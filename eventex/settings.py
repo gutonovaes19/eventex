d1f7e8853157adb00917463866919e5c1907fd49
gutonovaes19 <gutonovaes19@gmail.com>
2018-02-27 21:07:07 -0300

    Admin Django

--- eventex/settings.py
+++ eventex/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     'test_without_migrations',
     'django_extensions',
     'eventex.core',
-    'eventex.subscriptions',
+    'eventex.subscriptions.apps.SubscriptionsConfig',
 ]
 
 MIDDLEWARE = [
--- eventex/subscriptions/tests/test_model_subscription.py
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -20,3 +20,6 @@ class SubscriptionModelTest(TestCase):
         """Subscription must have an auto created at attr"""
         self.assertIsInstance(self.obj.created_at,datetime)
 
+    def test_str(self):
+        self.assertEqual('Henrique Bastos')
+
