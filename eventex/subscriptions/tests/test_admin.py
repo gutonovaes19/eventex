eb972d3d20044e597d615705e1ff429b128712cf
gutonovaes19 <gustavo@optionsinformatica.com.br>
2018-03-16 15:45:55 -0300

    mocado_mensagem

--- eventex/subscriptions/tests/test_admin.py
+++ eventex/subscriptions/tests/test_admin.py
@@ -1,6 +1,9 @@
+from unittest.mock import Mock
+
 from django.test import TestCase
+
 from eventex.subscriptions.admin import SubscriptionModelAdmin, Subscription, admin
-from eventex.subscriptions.dadosHb import dataHb
+
 
 #Proposta: criar uma classe onde preciso
 # 1o existe uma ação instalada
@@ -22,13 +25,47 @@ class SubscriptionModelAdminTest(TestCase):
 
     def test_mark_all(self):
         """it should mar all selected subscriptions as paid"""
+        Subscription.objects.create(name='Henrique Bastos',
+                                    cpf='12345678901',
+                                    email='henrique@bastos.net',
+                                    phone='21 - 996186180'
+                                    )
+        queryset = Subscription.objects.all()
+        #instancir o model_admin
+        model_admin = SubscriptionModelAdmin(Subscription, admin.site)  # instanciei
 
-        #Subscription.objects.create(name=dataHb.name,
-        ##                            cpf=dataHb.cpf,
-         #                           email=dataHb.email,
-         #                           phone=dataHb.phone)
-        #utra sintaxe, usando dicionátio, sugerida pelo regis.santos wttd
-        Subscription.objects.create(**dataHb)
+        mock = Mock()
+        old_message_user = SubscriptionModelAdmin.message_user
+        SubscriptionModelAdmin.message_user = mock
+
+        #chamar ation
+        model_admin.mark_as_paid(None,queryset)
+        #verificar o resultato
+        self.assertEqual(1, Subscription.objects.filter(paid=True).count())
+        SubscriptionModelAdmin.message_user = old_message_user
+
+
+    def test_message(self):
+        """It Should send a message to the user """
+        Subscription.objects.create(name='Henrique Bastos',
+                                    cpf='12345678901',
+                                    email='henrique@bastos.net',
+                                    phone='21 - 996186180')
+
+        model_admin = SubscriptionModelAdmin(Subscription, admin.site)
+        queryset = Subscription.objects.all()
+
+        mock = Mock()
+        old_message_user = SubscriptionModelAdmin.message_user
+        SubscriptionModelAdmin.message_user = mock
+
+        model_admin.mark_as_paid(None, queryset)
+
+        mock.assert_called_once_with(None,'1 inscrição foi marcada como paga.')
+
+        SubscriptionModelAdmin.message_user = old_message_user
+
+
 
 
 
